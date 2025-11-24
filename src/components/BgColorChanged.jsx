13bab870e471be17cf7eb816590d8485dc319fca
Tilak-csd <tilakgubhaju3@gmail.com>
2025-11-24 22:45:37 +0545

    assignment 2 completed

--- src/components/BgColorChanged.jsx
+++ src/components/BgColorChanged.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function BgColorChanged() {
-    const [colors, setColor] = useState(["green", "red", "blue", "black", "orange"]);
-    
+  const [bgcolor, setBgcolor] = useState("white")
+  const [colors, setColor] = useState(["green", "red", "blue", "black", "orange"]);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = bgcolor
+  }, [bgcolor])
+
   return (
     <div className='flex justify-center items-center absolute bottom-20 w-full gap-10 '>
-        {colors.map((color, index)=>{
-            if(color === "black" || color === "blue"){
+      {colors.map((color, index) => {
+        if (color === "black" || color === "blue") {
 
-                return <button className='rounded-[10px] py-[.5rem] px-[1rem] w-[5rem] cursor-pointer text-white' style={{background:`${color}`}} key={index} value={color}>{color}</button> 
-            }
-            return <button className='rounded-[10px] py-[.5rem] px-[1rem] w-[5rem] cursor-pointer' style={{background:`${color}`}} key={index} value={color}>{color}</button>
-        })}
+          return <button
+            onClick={() => setBgcolor(color)}
+            className='border-amber-50 rounded-[10px] py-[.5rem] px-[1rem] w-[5rem] cursor-pointer text-white' style={{ background: `${color}` }} key={index} value={color}>{color}</button>
+        }
+        return <button
+          onClick={() => setBgcolor(color)}
+          className='border-amber-50 rounded-[10px] py-[.5rem] px-[1rem] w-[5rem] cursor-pointer' style={{ background: `${color}` }} key={index} value={color}>{color}</button>
+      })}
     </div>
   )
 }
