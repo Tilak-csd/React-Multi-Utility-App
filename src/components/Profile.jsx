2d170621f2042e9c2b84129c4a8ac48e27f69758
Tilak-csd <tilakgubhaju3@gmail.com>
2025-11-24 16:35:12 +0545

    first assignment completed

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import profile from '../assets/images/profile.png'
 export default function Profile() {
     return (
-        <div>
+        <div className='w-[25%] '>
             <Background />
             <ProfileImage />
-            
+            <Social />
         </div>
     )
 }
@@ -20,5 +20,32 @@ function ProfileImage() {
         <img src={profile} alt="" className='rounded-[50%] w-[15rem]' />
         <p className='text-black font-bold text-[1.5rem] mt-[2rem]'>Rita Shrestha <span className='text-gray-400'>32</span></p>
         <p className='text-[1.5rem] text-gray-400 mt-[1rem]'>London</p>
+    </div>
+}
+
+function Social(){
+    return <div className='border-t-1 mt-[2rem] flex justify-around items-center p-[1rem]'>
+        <Followers />
+        <Likes />
+        <Posts />
+    </div>
+}
+
+function Followers(){
+    return <div>
+        <h1 className='text-2xl font-bold'>80K</h1>
+        <p className='text-gray-400 font-bold'>Followers</p>
+    </div>
+}
+function Likes(){
+    return <div>
+        <h1 className='text-2xl font-bold'>803K</h1>
+        <p className='text-gray-400 font-bold'>Likes</p>
+    </div>
+}
+function Posts(){
+    return <div>
+        <h1 className='text-2xl font-bold'>103</h1>
+        <p className='text-gray-400 font-bold'>Posts</p>
     </div>
 }
