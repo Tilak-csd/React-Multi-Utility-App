730a7da0b347eb4729a49aa37735f67e098e0295
Tilak-csd <tilakgubhaju3@gmail.com>
2025-11-28 14:05:55 +0545

    adding the image and starting to write the code

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Profile from './components/Profile'
 import ParagraphGenerator from './components/ParagraphGenerator'
 import Card from './components/Card'
 import OTPLogin from './components/OTPLogin'
+import BirthdayWish from './components/BirthdayWish'
 
 export default function App() {
   return (
-    <div>
+    <>
       {/* <Profile /> */}
       {/* <Card></Card> */}
 
       {/* <ParagraphGenerator /> */}
-      <OTPLogin></OTPLogin>
-    </div>
+      {/* <OTPLogin></OTPLogin> */}
+      <BirthdayWish />
+    </>
   )
 }
---
+++ src/components/BirthdayWish.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import background from '../assets/images/background.jpg'
+export default function BirthdayWish() {
+  return (
+    <div className='w-full h-[100vh]' style={{backgroundImage :`url(${background})`}}>
+      
+    </div>
+  )
+}
